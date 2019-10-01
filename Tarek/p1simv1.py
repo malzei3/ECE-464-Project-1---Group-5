1ee2439a483e6ea94d6a26cb12a7d23f4c78b1c1
Tarek Alzeiback <tarekalzeibak@yahoo.com>
2019-09-30 19:03:25 -0500

    Fixed a bug in part 1 and working on part 2

--- Tarek/p1simv1.py
+++ Tarek/p1simv1.py
@@ -53,6 +53,8 @@ def basic_sim(circuit):
 
         if term_has_value:
             circuit[curr][2] = True
+
+
             circuit = gateCalc(circuit, curr)
 
             # ERROR Detection if LOGIC does not exist
@@ -139,43 +141,58 @@ def fault_sim(circuit):
     queue = list(circuit["GATES"][1])
     i = 1
 
-    while True:
-        i -= 1
-        # If there's no more things in queue, done
-        if len(queue) == 0:
-            break
+    for line in cleanFaultList:
 
-        # Remove the first element of the queue and assign it to a variable for us to use
-        curr = queue[0]
-        queue.remove(curr)
-
-        # initialize a flag, used to check if every terminal has been accessed
-        term_has_value = True
-
-        # Check if the terminals have been accessed
-        for term in circuit[curr][1]:
-            if not circuit[term][2]:
-                term_has_value = False
+        while True:
+            i -= 1
+            # If there's no more things in queue, done
+            if len(queue) == 0:
                 break
 
-        if term_has_value:
-            circuit[curr][2] = True
-            circuit = gateCalc(circuit, curr)
+            # Remove the first element of the queue and assign it to a variable for us to use
+            curr = queue[0]
+            queue.remove(curr)
 
-            # ERROR Detection if LOGIC does not exist
-            if isinstance(circuit, str):
-                print(circuit)
-                return circuit
+            # initialize a flag, used to check if every terminal has been accessed
+            term_has_value = True
 
-            #print("Progress: updating " + curr + " = " + circuit[curr][3] + " as the output of " + circuit[curr][0] + " for:")
-            #for term in circuit[curr][1]:
-            #    print(term + " = " + circuit[term][3])
-            #print("\nPress Enter to Continue...")
-            #input()
+            # Check if the terminals have been accessed
+            for term in circuit[curr][1]:
+                if not circuit[term][2]:
+                    term_has_value = False
+                    break
 
-        else:
-            # If the terminals have not been accessed yet, append the current node at the end of the queue
-            queue.append(curr)
+            if term_has_value:
+                circuit[curr][2] = True
+
+                for term in circuit[curr][1]:
+                    inputWire = line[0]
+                    circuitInputs = circuit["INPUTS"][1]
+
+                    for w in circuitInputs:
+                        if w == inputWire:
+                            circuit[w][3] = line[1]
+
+                    if curr 
+
+
+
+                circuit = gateCalc(circuit, curr)
+
+                # ERROR Detection if LOGIC does not exist
+                if isinstance(circuit, str):
+                    print(circuit)
+                    return circuit
+
+                #print("Progress: updating " + curr + " = " + circuit[curr][3] + " as the output of " + circuit[curr][0] + " for:")
+                #for term in circuit[curr][1]:
+                #    print(term + " = " + circuit[term][3])
+                #print("\nPress Enter to Continue...")
+                #input()
+
+            else:
+                # If the terminals have not been accessed yet, append the current node at the end of the queue
+                queue.append(curr)
 
     return circuit
 
@@ -433,37 +450,20 @@ def generateFullFaultList():
             outputFile.write(x + "-SA-1" + "\n")
         numFault += 1
 
-    # Creating fault list for each input and outputs for all gates
-    for x in circuit["GATES"][1]:
-        z = 0
-        # Getting rid part of string to leave only line variable
-        if (x[0:5] == "wire_"):
-            x = x.replace("wire_", "")
-            outputFile.write(x + "-SA-0" + "\n")
-            outputFile.write(x + "-SA-1" + "\n")
-            # Changing Concatenate into str and getting rid of everything but wire variables
-            y = str(gateIn[i])
-            y = y.replace("['", "")
-            y = y.replace("']", "")
-            y = y.replace("', '", "")
-            inputNum = (len(y))     # number of variables to consider when going through str
-            numFault += 1
-            numFault = numFault + inputNum
+    gates = circuit["GATES"][1]
+    numberOfGates = len(gates)
 
-            # Taking each input variable and putting it into a SA-0 and SA-1 per gate
-            if inputNum > 1:
-                while inputNum >= 1:
-                    gateVar = y[z]
-                    inputNum -= 1
-                    outputFile.write(x + "-in-" + gateVar + "-SA-0" + "\n")
-                    outputFile.write(x + "-in-" + gateVar + "-SA-1" + "\n")
-                    z += 1
+    for line in gates:
+        outputFile.write(line + "-SA-0" + "\n")
+        outputFile.write(line + "-SA-1" + "\n")
+        numFault += 1
 
-            else:
-                # If there is only 1 input for gate just put it in SA-1 and SA-0
-                outputFile.write(y + "-in-" + x + "-SA-0" + "\n")
-                outputFile.write(y + "-in-" + x + "-SA-1" + "\n")
-            i += 1
+        gateINs = circuit[line][1]
+        for item in gateINs:
+             outputFile.write(line + "-IN-" + item + "-SA-0" + "\n")
+             outputFile.write(line + "-IN-" + item + "-SA-0" + "\n")
+             numFault += 1
+
     numFault = numFault*2 # multiply by 2 for 2 different fault states
     outputFile.write("\n" + "Total Fault: " + str(numFault))
 
@@ -512,7 +512,7 @@ def faultSimulation():
 
         circuit = inputRead(circuit, item)
         faultCircuit = copy.deepcopy(circuit)
-        inputFaultsRead()
+        #inputFaultsRead()
 
         circuit = copy.deepcopy(basic_sim(circuit))
         faultCircuit = copy.copy(fault_sim(faultCircuit))
@@ -634,10 +634,6 @@ def shiftBin(number, numberOfinputs):
              break
 
     return x
-
-
-
-
 
      
 # -------------------------------------------------------------------------------------------------------------------- #
